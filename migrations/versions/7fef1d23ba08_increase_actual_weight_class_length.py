ff8ad74dba542c84751c2fd784e0075095a9c084
UdsonWillams111 <udson.rego@invillia.com>
2025-11-12 19:51:11 -0300

    refactor: ajustar formatação e estilo do código na migração de aumento de comprimento da coluna actual_weight_class

--- migrations/versions/7fef1d23ba08_increase_actual_weight_class_length.py
+++ migrations/versions/7fef1d23ba08_increase_actual_weight_class_length.py
@@ -5,30 +5,36 @@ Revises: b1258634cdcf
 Create Date: 2025-11-12 19:37:31.994070
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '7fef1d23ba08'
-down_revision: Union[str, None] = 'b1258634cdcf'
+revision: str = "7fef1d23ba08"
+down_revision: Union[str, None] = "b1258634cdcf"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     # Aumentar o tamanho da coluna actual_weight_class de VARCHAR(50) para VARCHAR(100)
-    op.alter_column('fighters', 'actual_weight_class',
-                    type_=sa.String(100),
-                    existing_type=sa.String(50),
-                    existing_nullable=True)
+    op.alter_column(
+        "fighters",
+        "actual_weight_class",
+        type_=sa.String(100),
+        existing_type=sa.String(50),
+        existing_nullable=True,
+    )
 
 
 def downgrade() -> None:
     # Reverter para VARCHAR(50)
-    op.alter_column('fighters', 'actual_weight_class',
-                    type_=sa.String(50),
-                    existing_type=sa.String(100),
-                    existing_nullable=True)
+    op.alter_column(
+        "fighters",
+        "actual_weight_class",
+        type_=sa.String(50),
+        existing_type=sa.String(100),
+        existing_nullable=True,
+    )
