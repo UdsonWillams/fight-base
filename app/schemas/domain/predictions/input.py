1013bac0d7b9b773ee5422eba74a7f990911714c
Udson Willams <udson.willams@gmail.com>
2026-05-16 23:43:44 -0300

    feat: adicionar validador de campo ao modelo de previsão

--- app/schemas/domain/predictions/input.py
+++ app/schemas/domain/predictions/input.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from uuid import UUID
-from pydantic import BaseModel, Field, conint
+from pydantic import BaseModel, Field, conint, field_validator
 
 
 class PredictionInput(BaseModel):
